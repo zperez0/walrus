2c90e81afdab7798198cd1640f3e4f98791a61be
Yazmin Perez <zoeperez333@gmail.com>
2022-01-21 10:35:03 -0800

    add walrus-showing, walrus-hidden, toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
-jQuery(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a heading.");
-  });
-  
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-  
-  jQuery("img").click(function() {
-    alert("This is an image");
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
   });
 });
